2f9558d8aae7c14681cc46bc6d9c46170bc117c3
Oleksii Zubko <oleksii.zubko@gmail.com>
2015-05-11 18:57:21 +0300

    change username to oleksiizubko

--- scripts/initSetup.sh
+++ scripts/initSetup.sh
@@ -1,26 +1,26 @@
-# Set up user Oleksii.
+# Set up user oleksiizubko.
 
-# Create the user oleksii with password oleksii
-useradd -G sudo -p $(perl -e'print crypt("oleksii", "oleksii")') -m -s /bin/zsh oleksii
+# Create the user oleksiizubko with password oleksiizubko
+useradd -G sudo -p $(perl -e'print crypt("oleksiizubko", "oleksiizubko")') -m -s /bin/zsh oleksiizubko
 
-# Install oleksii keys
-mkdir -pm 700 /home/oleksii/.ssh
-curl -Lo /home/oleksii/.ssh/authorized_keys \
+# Install oleksiizubko keys
+mkdir -pm 700 /home/oleksiizubko/.ssh
+curl -Lo /home/oleksiizubko/.ssh/authorized_keys \
   'https://raw.githubusercontent.com/OleksiiZubko/id.rsa.pub/master/id_rsa.pub'
 
-curl -Lo /home/oleksii/.ssh/id_rsa.pub \
+curl -Lo /home/oleksiizubko/.ssh/id_rsa.pub \
   'https://raw.githubusercontent.com/OleksiiZubko/id.rsa.pub/master/id_rsa.pub'
 
-curl -Lo /home/oleksii/.ssh/id_rsa \
+curl -Lo /home/oleksiizubko/.ssh/id_rsa \
   'https://gist.githubusercontent.com/OleksiiZubko/cd1166ec68bb40fce278/raw/f1462397301e3173f665fa7211bca970ef4aa3f1/id_rsa'
 
-chmod 0600 /home/oleksii/.ssh/*
-chown -R oleksii:oleksii /home/oleksii/.ssh
+chmod 0600 /home/oleksiizubko/.ssh/*
+chown -R oleksiizubko:oleksiizubko /home/oleksiizubko/.ssh
 
-#install Oh My Zsh for root and for oleksii
+#install Oh My Zsh for root and for oleksiizubko
 git clone git@github.com:OleksiiZubko/oh-my-zsh.git ~/.oh-my-zsh
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 chsh -s /bin/zsh
-cp ~/.oh-my-zsh /home/oleksii/.oh-my-zsh
-chown oleksii:oleksii /home/oleksii/.oh-my-zsh
-cp /home/oleksii/.oh-my-zsh/templates/zshrc.zsh-template /home/oleksii/.oh-my-zsh/.zshrc
+cp ~/.oh-my-zsh /home/oleksiizubko/.oh-my-zsh
+chown oleksiizubko:oleksiizubko /home/oleksiizubko/.oh-my-zsh
+cp /home/oleksiizubko/.oh-my-zsh/templates/zshrc.zsh-template /home/oleksiizubko/.oh-my-zsh/.zshrc
